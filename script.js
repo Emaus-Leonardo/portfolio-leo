4b7ce7175fbfe701cf8b69ecb6672b2da1938ecf
Emaus-Leonardo <126693677+Emaus-Leonardo@users.noreply.github.com>
2024-04-16 21:26:07 -0300

    trocando animação do botão

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleMenu() {
 }
 
 var typed = new Typed(".auto-type", {
-    strings: ["LÉO", " A FRONT-END DEVELOPER", "A VIDEO EDITOR"],
+    strings: ["LÉO", " A FRONTEND DEVELOPER", "A VIDEO EDITOR"],
     typeSpeed: 100,
     backSpeed: 90,
     loop: true
